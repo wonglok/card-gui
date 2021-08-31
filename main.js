6ff7dadb6ce49afb8c07ea4e7209d8e0ca7a0b6f
wonglok <yellowhappy831@gmail.com>
2021-08-31 09:43:54 +0800

    deploy

--- main.js
+++ main.js
@@ -41,3 +41,60 @@ app.on("window-all-closed", function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// without Babel in ES2015
+const { NFC } = require("nfc-pcsc");
+
+const nfc = new NFC(); // optionally you can pass logger
+
+nfc.on("reader", (reader) => {
+  reader.on("card", async (card) => {
+    console.log();
+    console.log(`card detected`, card);
+
+    // example reading 12 bytes assuming containing text in utf8
+    try {
+      // reader.read(blockNumber, length, blockSize = 4, packetSize = 16)
+      const data = await reader.read(4, 12); // starts reading in block 4, continues to 5 and 6 in order to read 12 bytes
+      console.log(`data read`, data);
+      const payload = data.toString(); // utf8 is default encoding
+      console.log(`data converted`, payload);
+    } catch (err) {
+      console.error(`error when reading data`, err);
+    }
+  });
+
+  console.log(`${reader.reader.name}  device attached`);
+
+  // enable when you want to auto-process ISO 14443-4 tags (standard=TAG_ISO_14443_4)
+  // when an ISO 14443-4 is detected, SELECT FILE command with the AID is issued
+  // the response is available as card.data in the card event
+  // see examples/basic.js line 17 for more info
+  // reader.aid = 'F222222222';
+
+  reader.on("card", (card) => {
+    // card is object containing following data
+    // [always] String type: TAG_ISO_14443_3 (standard nfc tags like MIFARE) or TAG_ISO_14443_4 (Android HCE and others)
+    // [always] String standard: same as type
+    // [only TAG_ISO_14443_3] String uid: tag uid
+    // [only TAG_ISO_14443_4] Buffer data: raw data from select APDU response
+
+    console.log(`${reader.reader.name}  card detected`, card);
+  });
+
+  reader.on("card.off", (card) => {
+    console.log(`${reader.reader.name}  card removed`, card);
+  });
+
+  reader.on("error", (err) => {
+    console.log(`${reader.reader.name}  an error occurred`, err);
+  });
+
+  reader.on("end", () => {
+    console.log(`${reader.reader.name}  device removed`);
+  });
+});
+
+nfc.on("error", (err) => {
+  console.log("an error occurred", err);
+});
